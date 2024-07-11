506b3a0aa31b2df673eac5b6d2c42606c3b75235
Valery Semenenko <gearmobile@gmail.com>
2024-07-11 15:41:16 +0300

    add main animation

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Angular Balloon Game
+
+- [Balloon Game](https://youtu.be/yxEBBJ2Lsf0?si=u8cmAXUAax-twdq3)
--- src/app/components/balloon/balloon.component.ts
+++ src/app/components/balloon/balloon.component.ts
@@ -1,5 +1,13 @@
-import { ChangeDetectionStrategy, Component, input } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  ElementRef,
+  inject,
+  input,
+  OnInit,
+} from '@angular/core';
 import { IBalloon } from '../../interfaces';
+import { animate, AnimationBuilder, style } from '@angular/animations';
 
 @Component({
   selector: 'bl-balloon',
@@ -9,6 +17,23 @@ import { IBalloon } from '../../interfaces';
   styleUrl: './balloon.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class BalloonComponent {
+export class BalloonComponent implements OnInit {
+  private readonly animation = inject(AnimationBuilder);
+  private readonly elementRef = inject(ElementRef);
+
   public balloon = input.required<IBalloon>();
+
+  ngOnInit(): void {
+    this.generateAnimation();
+  }
+
+  private generateAnimation(): void {
+    const animation = this.animation.build([
+      style({ translate: `0 0`, position: 'fixed', left: 0, bottom: 0 }),
+      animate(`4s ease-in-out`, style({ translate: `0 -100vh` })),
+    ]);
+    const player = animation.create(this.elementRef.nativeElement.firstChild);
+    player.play();
+    player.onDone(() => console.log('all done'));
+  }
 }
